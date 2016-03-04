f0472ca65cdf3c6031b21edfafa68ce9b3eb3ed9
klod1888 <nemchik9000@yahoo.com>
2016-03-04 14:12:06 +0300

    Added files via upload

---
+++ turing-maschine.cpp
@@ -0,0 +1,14 @@
+#include "turing-maschine.h"
+#include "ui_turing-maschine.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ turing-maschine.h
@@ -0,0 +1,22 @@
+#ifndef TURINGMASCHINE_H
+#define TURINGMASCHINE_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // TURINGMASCHINE_H
